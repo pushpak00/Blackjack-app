d658e470a98d3b7c2b93c051898557d4a93b7b7d
Pushpak Paunikar <54863992+pushpak00@users.noreply.github.com>
2025-01-10 18:25:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstCard = 3
+let secondCard = 9
+let sum = firstCard + secondCard
+let hasBlackjack = false;
+let isAlive = true;
+
+let message = ""
+
+if (sum <= 20){
+    message = "Do you want to draw a new card? 🙂"
+}
+else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackjack = true;
+}
+else {
+    message = "You're out of the game! 😭"
+    isAlive = false;
+}
