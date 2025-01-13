364b13031ea0462f868e046ca39502acc0e737db
Pushpak Paunikar <54863992+pushpak00@users.noreply.github.com>
2025-01-13 18:53:43 +0530

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-let firstCard = 3
-let secondCard = 9
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false;
 let isAlive = true;
-
 let message = ""
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (sum <= 20){
-    message = "Do you want to draw a new card? 🙂"
+function startGame() {
+    renderGame();
 }
-else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackjack = true;
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0 ; i < cards.length ; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = `Sum: ${sum}`
+    if (sum <= 20) { 
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true;
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
 }
-else {
-    message = "You're out of the game! 😭"
-    isAlive = false;
+
+function newCard() {
+    
+    console.log("Drawing a new card from the deck!")
+    let card = 7
+    sum += card 
+    cards.push(card)
+    startGame()
 }
