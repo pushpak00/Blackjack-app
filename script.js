b0bbe92966b2344f1c814e29864d62a53b85ad4d
Pushpak Paunikar <54863992+pushpak00@users.noreply.github.com>
2025-01-14 16:18:52 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Gandalf",
+    chips: 125
+}
+
+let cards = []
+let sum = 0
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = ""
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber =  Math.floor( Math.random() * 13) + 1
+
+    if (randomNumber > 10) {
+        return 10
+    }
+    else if (randomNumber === 1) {
+        return 11
+    }
+    else {
+        return randomNumber
+    }
+
+}
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [ firstCard, secondCard ]
+    sum = firstCard + secondCard
     renderGame();
 }
 function renderGame() {
@@ -36,9 +61,12 @@ function renderGame() {
 
 function newCard() {
     
-    console.log("Drawing a new card from the deck!")
-    let card = 7
+    if (isAlive === true && hasBlackjack === false){
+
+    let card = getRandomCard()
     sum += card 
     cards.push(card)
-    startGame()
+    renderGame()
+
+    }
 }
